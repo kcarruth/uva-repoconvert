11050c6b6e6cc56d8f1077111ff851e3383d64f6
Kevin Carruth <kbc6f@virginia.edu>
2018-02-02 11:54:15 -0500

    - added exclusion of commits that are just svnmerge tracking (directory props) - sorted tool processing order

--- sanitycheck.sh
+++ sanitycheck.sh
@@ -9,6 +9,16 @@ REPODIR="$WORKDIR/combined"
 
 SVNROOT="svn+ssh://atgsvn.itc.virginia.edu/sakai"
 
+declare -a IGNOREMSGS
+IGNOREMSGS=(
+	"initializing svnmerge from"
+	"uninitializing old svnmerge watches from"
+	"Initialized merge tracking via \"svnmerge\""
+	"Removed merge tracking for \"svnmerge\""
+	"hiding [a-z]+ target dirs from svn"
+	"hiding target directories from svn"
+)
+
 if [[ $1 ]]; then
 	REPOS="$1"
 else
@@ -20,9 +30,9 @@ svntools=$( svn ls $SVNROOT/uva-collab | sed "s|/$||" )
 # verify top poms and last commits match up
 for gitrepo in $REPOS; do
 
-	echo ">"
-	echo "> starting $gitrepo"
-	echo ">"
+	echo ""
+	echo "starting $gitrepo"
+	echo "=============================="
 
 	if [[ ! -d $REPODIR/$gitrepo/.git ]]; then
 		echo "error: $gitrepo is not a valid git repo"
@@ -40,14 +50,44 @@ for gitrepo in $REPOS; do
 			echo "MISSING TOP POM: $gitrepo / $gitbranch"
 		fi
 
-		svnbranch="sakai_${svnversion}_${gitbranch}"
 
-		for tool in $( find . -maxdepth 1 -type d | grep -v "\.git" | egrep -v "^\.$" | sed "s|^\./||" ); do
+		for tool in $( find . -maxdepth 1 -type d | grep -v "\.git" | egrep -v "^\.$" | sed "s|^\./||" | sort ); do
 
 			if [[ $( svn ls $SVNROOT/uva-collab/$tool/branches 2>/dev/null ) ]]; then
 
+				svnbranch="$SVNROOT/uva-collab/$tool/branches/sakai_${svnversion}_${gitbranch}"
+
 				last_gitcommit=$( git log -1 $tool | grep "git-svn-id" | sed -r "s/^[^@]*@([0-9]+) .*$/\1/" )
-				last_svncommit=$( svn log -q -l 1 $SVNROOT/uva-collab/$tool/branches/$svnbranch | egrep "^r" | sed -r "s/^r([0-9]+) \|.*$/\1/" )
+				last_svncommit=$( svn log -q -l 1 $svnbranch | egrep "^r" | sed -r "s/^r([0-9]+) \|.*$/\1/" )
+
+				if [[ $last_gitcommit -eq $last_svncommit ]]; then
+					# if they match, no need to keep checking
+					continue
+				fi
+
+				# git conversion ignored svnmerge inits and the like, so accomodate
+				found_last_svncommit="0"
+				svn_backlog="2"
+				while [ $found_last_svncommit -eq 0 ]; do
+					declare -a svnrevs					
+					mapfile -t svnrevs < <( svn log -q -l $svn_backlog $svnbranch | egrep "^r" | sed -r "s/^r([0-9]+) \|.*$/\1/" )
+					svnrev=${svnrevs[-1]}
+
+					ignorecommit="0"
+					for imsg in "$IGNOREMSGS"; do
+						if svn log $svnbranch -r$svnrev | egrep "$imsg"; then
+							ignorecommit="1"
+						fi
+					done
+
+					# check for svnmerge
+					if [[ $ignorecommit -eq 1 ]]; then
+						svn_backlog=$[$svn_backlog+1]
+					else
+						found_last_svncommit="1"
+						last_svncommit="$svnrev"
+					fi
+				done
 
 				#echo "git: $last_gitcommit"
 				#echo "svn: $last_svncommit"
@@ -66,10 +106,6 @@ for gitrepo in $REPOS; do
 
 	# verify each appropriate tool got imported and combined
 	for svntool in $svntools; do 
-
-		#echo ">>"
-		#echo ">> $gitrepo / $svntool"
-		#echo ">>"
 
 		for svnbranch in $( svn ls $SVNROOT/uva-collab/$svntool/branches | grep "sakai_${svnversion}_" ); do
 
