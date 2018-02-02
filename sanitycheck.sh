221e08a9c011b5706cc6ddfc68c3398bb47692b0
Kevin Carruth <kbc6f@virginia.edu>
2018-02-02 10:19:51 -0500

    log clarifications, excluding svn tools w/ no branches, excluding .git dirs

--- sanitycheck.sh
+++ sanitycheck.sh
@@ -20,6 +20,10 @@ svntools=$( svn ls $SVNROOT/uva-collab | sed "s|/$||" )
 # verify top poms and last commits match up
 for gitrepo in $REPOS; do
 
+	echo ">"
+	echo "> starting $gitrepo"
+	echo ">"
+
 	if [[ ! -d $REPODIR/$gitrepo/.git ]]; then
 		echo "error: $gitrepo is not a valid git repo"
 		continue
@@ -28,7 +32,6 @@ for gitrepo in $REPOS; do
 	cd $REPODIR/$gitrepo
 	svnversion=$( echo $gitrepo | sed "s/\./-/g" )
 
-	if [[ "1" == "0" ]]; then
 	for gitbranch in $( git branch | sed "s/^..//" ); do
 
 		git checkout $gitbranch 2>/dev/null
@@ -39,31 +42,34 @@ for gitrepo in $REPOS; do
 
 		svnbranch="sakai_${svnversion}_${gitbranch}"
 
-		for tool in $( find . -maxdepth 1 -type d | egrep -v "^\.$" | sed "s|^\./||" ); do
+		for tool in $( find . -maxdepth 1 -type d | grep -v "\.git" | egrep -v "^\.$" | sed "s|^\./||" ); do
 
-			last_gitcommit=$( git log -1 $tool | grep "git-svn-id" | sed -r "s/^[^@]*@([0-9]+) .*$/\1/" )
-			last_svncommit=$( svn log -q -l 1 $SVNROOT/uva-collab/$tool/branches/$svnbranch | egrep "^r" | sed -r "s/^r([0-9]+) \|.*$/\1/" )
+			if [[ $( svn ls $SVNROOT/uva-collab/$tool/branches 2>/dev/null ) ]]; then
 
-			echo "git: $last_gitcommit"
-			echo "svn: $last_svncommit"
+				last_gitcommit=$( git log -1 $tool | grep "git-svn-id" | sed -r "s/^[^@]*@([0-9]+) .*$/\1/" )
+				last_svncommit=$( svn log -q -l 1 $SVNROOT/uva-collab/$tool/branches/$svnbranch | egrep "^r" | sed -r "s/^r([0-9]+) \|.*$/\1/" )
 
-			if [[ "$last_gitcommit" != "$last_svncommit" ]]; then
-				echo "REV MISMATCH FOUND: $gitrepo / $gitbranch / $tool"
-				echo "> last git rev: $last_gitcommit"
-				echo "> last svn rev: $last_svncommit"
-			fi
+				#echo "git: $last_gitcommit"
+				#echo "svn: $last_svncommit"
+
+				if [[ "$last_gitcommit" != "$last_svncommit" ]]; then
+					echo "REV MISMATCH FOUND: $gitrepo / $gitbranch / $tool"
+					echo "> last git rev: $last_gitcommit"
+					echo "> last svn rev: $last_svncommit"
+				fi
+
+			fi # end if /branches exists in svn
 
 		done # end foreach tool
 
 	done # end foreach git branch
-	fi # shitty block comment
 
 	# verify each appropriate tool got imported and combined
 	for svntool in $svntools; do 
 
-		echo ">>"
-		echo ">> $gitrepo / $svntool"
-		echo ">>"
+		#echo ">>"
+		#echo ">> $gitrepo / $svntool"
+		#echo ">>"
 
 		for svnbranch in $( svn ls $SVNROOT/uva-collab/$svntool/branches | grep "sakai_${svnversion}_" ); do
 
@@ -77,7 +83,7 @@ for gitrepo in $REPOS; do
 					echo "MISSING TOOL: $svntool not found in $gitrepo / $gitbranch"
 				fi
 			else 
-				echo "MISSING BRANCH: $gitbranch missing from $gitrepo"
+				echo "MISSING BRANCH: $svntool/$gitbranch missing from $gitrepo"
 			fi			
 
 		done # end each svnbranch		
