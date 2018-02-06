21489d4cdc24a14567721c87906718f4b14d0e05
Kevin Carruth <kbc6f@virginia.edu>
2018-02-06 15:51:16 -0500

    fixing indents

--- config/migrate.sh
+++ config/migrate.sh
@@ -12,10 +12,6 @@ INIT_OPTS=""
 FETCH_OPTS="--no-follow-parent --authors-file=$AUTHORSFILE"
 CLONE_OPTS="$INIT_OPTS $FETCH_OPTS"
 
-if [[ ! -d $WORKDIR/branches ]]; then
-	mkdir -p $WORKDIR/branches
-fi
-
 svnbranches=""
 # get list of branches to migrate
 for b in $( svn ls $SVNROOT/branches | sed -r "s|/$||" ); do
@@ -26,38 +22,30 @@ for b in $( svn ls $SVNROOT/branches | sed -r "s|/$||" ); do
 done
 
 
-#for svnbranch in $svnbranches; do
+#git svn clone $CLONE_OPTS --trunk="branches/sakai_11-x_$svnbranch" "$SVNROOT" $WORKDIR/branches/$svnbranch
+# migrate tool
+git svn init $INIT_OPTS --branches=REPLACEHERE $SVNROOT $WORKDIR/fromsvn
 
-#	if [[ -d $WORKDIR/branches/$svnbranch ]]; then
-#		# already done? skip
-#		continue
-#	fi
+cd "$WORKDIR/fromsvn"
+sed -i "s|REPLACEHERE/\*|branches/\{$svnbranches\}/properties|" .git/config
 
-	#git svn clone $CLONE_OPTS --trunk="branches/sakai_11-x_$svnbranch" "$SVNROOT" $WORKDIR/branches/$svnbranch
-	# migrate tool
-	git svn init $INIT_OPTS --branches=REPLACEHERE $SVNROOT $WORKDIR/fromsvn
-	
-	cd "$WORKDIR/fromsvn"
-	sed -i "s|REPLACEHERE/\*|branches/\{$svnbranches\}/properties|" .git/config
-	
-	git svn fetch $FETCH_OPTS
+git svn fetch $FETCH_OPTS
 
-	# generate branches
-	git for-each-ref --format="%(refname:short) %(objectname)" refs/remotes/origin |
-	while read branch ref; do
-	    gitbranch=`echo $branch | sed "s|origin/sakai_11-x_||g"`
-	    git branch $gitbranch $ref
-	
-		# in-branch work
-		git checkout $gitbranch
+# generate branches
+git for-each-ref --format="%(refname:short) %(objectname)" refs/remotes/origin |
+while read branch ref; do
+    gitbranch=`echo $branch | sed "s|origin/sakai_11-x_||g"`
+    git branch $gitbranch $ref
 
-		# move down one two levels
-		git filter-branch -f --index-filter 'git ls-files -s | sed "s#\t\"*#&'local/"$gitbranch"'/#" | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && if [ -f $GIT_INDEX_FILE.new ]; then mv $GIT_INDEX_FILE.new $GIT_INDEX_FILE; fi' HEAD
-	
-	done # end while read branch	
+	# in-branch work
+	git checkout $gitbranch
 
-	git branch -d master
-#done # end foreach svnbranch
+	# move down one two levels
+	git filter-branch -f --index-filter 'git ls-files -s | sed "s#\t\"*#&'local/"$gitbranch"'/#" | GIT_INDEX_FILE=$GIT_INDEX_FILE.new git update-index --index-info && if [ -f $GIT_INDEX_FILE.new ]; then mv $GIT_INDEX_FILE.new $GIT_INDEX_FILE; fi' HEAD
+
+done # end while read branch	
+
+git branch -d master
 
 TIME=$( expr $(date +%s) - $START )
 echo "Finished in $TIME seconds at $( date +%F\ %T )"
