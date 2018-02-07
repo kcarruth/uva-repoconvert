f53180f8afff23da333ba37e1d8189fc0cc009ab
Kevin Carruth <kbc6f@virginia.edu>
2018-02-07 14:28:27 -0500

    misc repo split/migration

---
+++ misc/migrate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# migrate.sh
+
+BASEDIR="/home/vagrant/repoconv"
+WORKDIR="$BASEDIR/work/misc"
+
+INITOPTS=""
+FETCHOPTS="--no-follow-parent --authors-file=$BASEDIR/authors/combined.txt"
+CLONEOPTS="$INIT_OPTS $FETCH_OPTS"
+
+SVNROOT="svn+ssh://atgsvn.itc.virginia.edu/misc"
+
+trunks=$( svn ls $SVNROOT | sed "s|/$||" )
+
+for trunk in $trunks; do
+
+	git svn clone $CLONEOPTS --trunk="$trunk" $SVNROOT $WORKDIR/$trunk
+
+done
